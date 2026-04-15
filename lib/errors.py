97fa6a9a037267f2d1a197b6b9d37e2e93488100
Duncan Robertson <duncanr19@gmail.com>
2026-04-15 11:18:54 +0100

    Revert uniform genotype error addition

--- lib/errors.py
+++ lib/errors.py
@@ -66,19 +66,6 @@ def add_empirical_genotype_errors(G_in, rng, probs_func, **kwargs):
         G_out[site, :, :] = sample_genotypes_vectorised(g, probs, rng)
     return G_out
 
-def add_uniform_genotype_errors(G_in, rng, unifgeno):
-    """
-    Independently flip each haploid allele with probability unifgeno.
-    """
-    assert 0 <= unifgeno <= 1
-    assert len(np.unique(G_in)) <= 2
-    assert len(G_in.shape) == 3
-    assert G_in.shape[2] == 2
-    assert G_in.dtype == np.int8
-
-    include_flip = rng.random(G_in.shape) < unifgeno
-    return np.bitwise_xor(G_in, include_flip.astype(np.int8))
-
 @njit
 def phase_switch_diplotype(d_in, d_out, phase_array, switch_sites):
     phase = 0
@@ -164,7 +151,6 @@ def add_errors(
     output_path,
     error_csv_path,
     empirical_errors_enabled,
-    unifgeno,
     phase_error_rate,
     mispol_error_rate,
     seed,
@@ -185,8 +171,6 @@ def add_errors(
         G_geno_error = G_in
     else:
         raise ValueError("Invalid empirical error setting specified")
-    if unifgeno > 0:
-        G_geno_error = add_uniform_genotype_errors(G_geno_error, rng, unifgeno)
     error_mask = G_geno_error == G_in
     genotype_error_count = np.sum(~error_mask, axis=(1,2))
     add_xarray(new_vars, error_mask, dims=["variants", "samples", "ploidy"], name="call_genotype_error_mask")
